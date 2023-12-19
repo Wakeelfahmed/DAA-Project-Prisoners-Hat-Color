9a575cc6a0e32076a1b4e9619d34ae389d21d04c
Wakeel Furqan Ahmed <wakeelfahmed@gmail.com>
2023-12-19 17:43:23 +0500

    Improved output Information & output formatting

--- Hat Colors/Source.cpp
+++ Hat Colors/Source.cpp
@@ -2,20 +2,12 @@
 #include <vector>
 using namespace std;
 enum HatColors { BLACK, WHITE };
-class Tester;
 class Prisoner {
-	friend class Tester;
 public:
 	HatColors hatColor;
-	bool steppedForward;	//bool caused_testfail;
+	bool steppedForward;
 	Prisoner() : hatColor(WHITE), steppedForward(false) {}
 	Prisoner(HatColors color) : hatColor(color), steppedForward(false) {}
-};
-class Tester
-{
-private:
-	void BlackHat(vector<Prisoner>& prisoners);
-public:
 };
 
 void passTest(vector<Prisoner>& prisoners) {
@@ -23,16 +15,17 @@ void passTest(vector<Prisoner>& prisoners) {
 	for (int Prisoners_loop = 0; Prisoners_loop < prisoners.size(); ++Prisoners_loop) {
 		BlackHats_count = 0;
 		// Count the number of black hats among other prisoners
-		for (int scan_loop = 0; scan_loop < prisoners.size(); ++scan_loop) {
+		for (int scan_loop = 0; scan_loop < prisoners.size(); ++scan_loop)
 			if (scan_loop != Prisoners_loop && prisoners[scan_loop].hatColor == BLACK)
 				BlackHats_count++;
-		}
+
 		if (BlackHats_count > Max_Black_Hat_Founds)
 			Max_Black_Hat_Founds = BlackHats_count;
+
 		// Case 1: 1 Black Hat
 		if (BlackHats_count == 0) {
 			prisoners[Prisoners_loop].steppedForward = true;
-			//cout << "Smart Prisoners ESCAPED !!!" << endl;
+			cout << "P" << Prisoners_loop + 1 << "\t";
 			return;
 		}
 	}
@@ -43,16 +36,14 @@ void passTest(vector<Prisoner>& prisoners) {
 		for (int Prisoners_loop = 0; Prisoners_loop < prisoners.size(); ++Prisoners_loop) {
 			int BlackHatsSeen = 0;
 			// Count the number of black hats seen by this prisoner in the current lineup
-			for (int scan_loop = 0; scan_loop < prisoners.size(); ++scan_loop) {
-				if (scan_loop != Prisoners_loop && prisoners[scan_loop].hatColor == BLACK) {
+			for (int scan_loop = 0; scan_loop < prisoners.size(); ++scan_loop)
+				if (scan_loop != Prisoners_loop && prisoners[scan_loop].hatColor == BLACK)
 					BlackHatsSeen++;
-				}
-			}
 
 			// Check if the current prisoner can deduce the color of their hat
 			if (BlackHatsSeen == currentLineup - 1) {
 				prisoners[Prisoners_loop].steppedForward = true;
-				//break;
+				cout << "P" << Prisoners_loop + 1 << " ";
 			}
 		}
 		currentLineup++;
@@ -61,34 +52,27 @@ void passTest(vector<Prisoner>& prisoners) {
 bool Jailer_Test(const vector<Prisoner> prisoners) {
 	bool Test = true;
 	for (Prisoner p : prisoners)
-	{
 		if (p.hatColor == WHITE && p.steppedForward)
 			Test = false;
-		if (p.hatColor == BLACK && p.steppedForward == false)
+		else if (p.hatColor == BLACK && p.steppedForward == false)
 			Test = false;
-	}
+
 	if (Test)
-		cout << "ESCAPED" << endl;
+		cout << "\t\t\t\tESCAPED" << endl;
 	else
-		cout << "FAILED, Excecuted" << endl;
+		cout << "\t\t\t\tFAILED, Excecuted" << endl;
 	return Test;
 }
 int main() {
 	const int numPrisoners = 12;
 	std::vector<Prisoner> Testprisoners(numPrisoners);
-
+	cout << "\x1b[3;96mPrisoners Combination\t\tStepped Forward\t\t\tWarden Result\033[0m" << endl;
 	// Generate all possible combinations
 	for (int i = 0; i < (1 << numPrisoners); ++i) {
 		for (int j = 0; j < numPrisoners; ++j) {
 			Testprisoners[j].hatColor = ((i >> j) & 1) ? BLACK : WHITE;
 			Testprisoners[j].steppedForward = false;
 		}
-		for (Prisoner p : Testprisoners)
-			if (p.hatColor == BLACK)
-				cout << "B ";
-			else
-				cout << "W ";
-		cout << endl;
 		int allwhite = 0;
 		for (Prisoner p : Testprisoners)
 			if (p.hatColor == WHITE)
@@ -102,30 +86,15 @@ int main() {
 		if (allblack == 12 || allwhite == 12)
 			continue;	//dont run test
 
-		//prisoners[0].hatColor = BLACK;
-		//prisoners[1].hatColor = BLACK;
-		//prisoners[2].hatColor = BLACK;
-		//prisoners[3].hatColor = BLACK;
-		//prisoners[4].hatColor = BLACK;
-		//prisoners[5].hatColor = BLACK;
-		//prisoners[6].hatColor = BLACK;
-		//prisoners[7].hatColor = BLACK;
-		//prisoners[8].hatColor = BLACK;
-		//prisoners[9].hatColor = BLACK;
-		//prisoners[10].hatColor = BLACK;
-		//prisoners[11].hatColor = BLACK;
-		passTest(Testprisoners);
+		for (Prisoner p : Testprisoners)
+			if (p.hatColor == BLACK)
+				cout << "B ";
+			else
+				cout << "W ";
+		cout << "   ";
 
-		//passTest(prisoners);
+		passTest(Testprisoners);	//The Algo
 
-		// Check results
-		//for (int i = 0; i < prisoners.size(); ++i) {
-		//	cout << "Prisoner " << i << ": " << (prisoners[i].steppedForward ? "Stepped FORWARD" : "Stayed in line");
-		//	if (prisoners[i].steppedForward && prisoners[i].hatColor == WHITE)
-		//		cout << "  but color white";
-		//	else
-		//		cout << endl;
-		//}
-		Jailer_Test(Testprisoners);
+		Jailer_Test(Testprisoners); //Wardens checks, and Successful (escaped) if & only if ALL BLACKS STEPPED FORWARD
 	}
 }
